884fc6ef5c01f5cb2633cfcb582ea9bfbd0ace15
mohamed bourra <itzmohamed007@gmail.com>
2024-05-28 23:43:04 +0100

    added contact model

---
+++ larakech/app/Models/Contact.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Contact extends Model
+{
+    use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillables = ['cle', 'organisation_id', 'e_mail', 'nom', 'prenom', 'telephon_fixe', 'service', 'fonction'];
+
+    /**
+     * Get the owning organisation of the contact
+     *
+     * @return array<string, string>
+     */
+    public function organisation(): BelongsTo
+    {
+        return $this->belongsTo(Organisation::class);
+    }
+}
