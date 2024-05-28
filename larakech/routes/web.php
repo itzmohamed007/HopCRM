bbeba64bc6b06fa86aa3bdbcb58052d54c3f6a51
mohamed bourra <itzmohamed007@gmail.com>
2024-05-28 23:49:03 +0100

    registered index (get all) method to its endpoint

--- larakech/routes/web.php
+++ larakech/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\OrganisationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', [ContactController::class, 'index']);
