f90a481e3e92866469a37fe0ae3e01ecd8ca4dc0
mohamed bourra <itzmohamed007@gmail.com>
2024-05-30 02:25:47 +0100

    registered new endpoints and added security message endpoints for 404 cases

---
+++ larakech/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\OrganisationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('v1/contacts', [ContactController::class, 'index']);
+Route::get('v1/contacts/{id}', [ContactController::class, 'show']);
+Route::post('v1/contacts', [ContactController::class, 'store']);
+Route::put('v1/contacts', [ContactController::class, 'update']);
+Route::delete('v1/contacts', [ContactController::class, 'destroy']);
+
+Route::post('v1/contacts/duplicate', [ContactController::class, 'isDuplicate']);
+Route::post('v1/organisations/duplicate', [OrganisationController::class, 'isDuplicate']);
+
+Route::fallback(function(){
+    return response()->json([
+        'message' => 'Route Not Found'], 404);
+});
