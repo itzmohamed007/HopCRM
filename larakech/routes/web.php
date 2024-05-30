aff2bf7dbb7aaa771215b9ac9340734ba4d03667
mohamed bourra <itzmohamed007@gmail.com>
2024-05-30 02:25:17 +0100

    cleaned web routing file

--- larakech/routes/web.php
+++ larakech/routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\OrganisationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', [ContactController::class, 'index']);
