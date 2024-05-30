814e3e41a66585dda36eb38dbb84a926ba83db62
mohamed bourra <itzmohamed007@gmail.com>
2024-05-30 02:19:45 +0100

    formating default exception handling for ModelNotFoundException

--- larakech/bootstrap/app.php
+++ larakech/bootstrap/app.php
@@ -1,12 +1,15 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ModelNotFoundException $e, Request $request) {
+            if ($request->wantsJson()) {
+                return response()->json([
+                    'error' => 'Entry for ' . str_replace('App', '', $e->getModel()) . ' not found'],
+                    404
+                );
+            }
+        });
     })->create();
