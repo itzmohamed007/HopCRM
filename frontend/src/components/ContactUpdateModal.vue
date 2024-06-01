80efcf1d3a22f8a6cde6975909d4ade37d9b0b1b
mohamed bourra <itzmohamed007@gmail.com>
2024-06-01 17:44:58 +0100

    removed readonly attributes from contact update modal

--- frontend/src/components/ContactUpdateModal.vue
+++ frontend/src/components/ContactUpdateModal.vue
@@ -11,45 +11,45 @@
                         <div class="flex space-x-4">
                             <div class="flex-1">
                                 <label class="block text-sm font-medium text-gray-600">Prénom</label>
-                                <input v-model="localContact.prenom" type="text" readonly
+                                <input v-model="localContact.prenom" type="text"
                                     class="focus:border focus:border-black mt-1 px-3 py-2 text-sm w-full border border-gray-300 rounded-md">
                             </div>
                             <div class="flex-1">
                                 <label class="block text-sm font-medium text-gray-600">Nom</label>
-                                <input v-model="localContact.nom" type="text" readonly
+                                <input v-model="localContact.nom" type="text"
                                     class="focus:border focus:border-black mt-1 px-3 py-2 text-sm w-full border border-gray-300 rounded-md">
                             </div>
                         </div>
                         <div>
                             <label class="block text-sm font-medium text-gray-600">E-mail</label>
-                            <input v-model="localContact.e_mail" type="text" readonly
+                            <input v-model="localContact.e_mail" type="text"
                                 class="focus:border focus:border-black mt-1 px-3 py-2 text-sm w-full border border-gray-300 rounded-md">
                         </div>
                         <div>
                             <label class="block text-sm font-medium text-gray-600">Entreprise</label>
-                            <input v-model="localContact.organisation.nom" type="text" readonly
+                            <input v-model="localContact.organisation.nom" type="text"
                                 class="focus:border focus:border-black mt-1 px-3 py-2 text-sm w-full border border-gray-300 rounded-md">
                         </div>
                         <div>
                             <label class="block text-sm font-medium text-gray-600">Adresse</label>
-                            <textarea v-model="localContact.organisation.addresse" type="text" readonly
+                            <textarea v-model="localContact.organisation.addresse" type="text"
                                 class="focus:border focus:border-black mt-1 px-3 py-2 text-sm w-full border border-gray-300 rounded-md resize-none h-20"></textarea>
                         </div>
                         <div class="flex space-x-4">
                             <div class="w-3/12">
                                 <label class="block text-sm font-medium text-gray-600">Code postal</label>
-                                <input v-model="localContact.organisation.code_postal" type="text" readonly
+                                <input v-model="localContact.organisation.code_postal" type="text"
                                     class="focus:border focus:border-black mt-1 px-3 py-2 text-sm w-full border border-gray-300 rounded-md">
                             </div>
                             <div class="w-full">
                                 <label class="block text-sm font-medium text-gray-600">Ville</label>
-                                <input v-model="localContact.organisation.ville" type="text" readonly
+                                <input v-model="localContact.organisation.ville" type="text"
                                     class="focus:border focus:border-black mt-1 px-3 py-2 text-sm w-full border border-gray-300 rounded-md">
                             </div>
                         </div>
                         <div>
                             <label class="block text-sm font-medium text-gray-700">Statut</label>
-                            <input v-model="localContact.organisation.statut" type="text" readonly
+                            <input v-model="localContact.organisation.statut" type="text"
                                 class="focus:border focus:border-black mt-1 px-3 py-2 text-sm w-full border border-gray-300 rounded-md">
                         </div>
                     </div>
