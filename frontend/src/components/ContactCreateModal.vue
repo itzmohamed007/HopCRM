733c8a608235860adbefddc0a605c9865ab510f9
mohamed bourra <itzmohamed007@gmail.com>
2024-06-01 22:30:48 +0100

    modified contact create modal logic to be suitable with new (duplicates check) step

--- frontend/src/components/ContactCreateModal.vue
+++ frontend/src/components/ContactCreateModal.vue
@@ -61,7 +61,7 @@
                     <div class="p-5 bg-gray-100 flex items-center justify-end gap-2 text-sm rounded-b-md">
                         <button @click="$emit('closeCreateModal')"
                             class="px-5 py-2 rounded-md text-gray-900 bg-gray-100 border border-gray-300">Annuler</button>
-                        <button @click="create()"
+                        <button @click="handleRequestCreateContact()"
                             class="px-5 py-2 rounded-md text-white bg-customCyan border border-gray-300">Valider</button>
                     </div>
                 </form>
@@ -71,7 +71,6 @@
 </template>
 
 <script>
-import axios from 'axios';
 export default {
     name: "ContactCreateModal",
     data() {
@@ -85,21 +84,50 @@ export default {
         };
     },
     methods: {
-        async create() {
-            try {
-                this.errors = []
-                const response = await axios.post('/contacts', this.contact);
-                if (response.status == 201)
-                    this.$emit('fetch')
-            } catch (error) {
-                this.errors = []
-                Object.keys(error.response.data.errors).forEach(key => {
-                    this.errors.push(error.response.data.errors[key][0])
-                })
-                this.displayErrorsBox = true
-                console.log('an error occured while create a contact')
-                console.log(error)
-            }
+        handleRequestCreateContact() {
+            this.displayErrorsBox = false;
+            this.validateDuplicateSearchInputs()
+            if (!this.errors.length)
+                this.$emit('requestCreateContact', this.contact)
+            else
+                this.displayErrorsBox = true;
+        },
+        validateDuplicateSearchInputs() {
+            const alphaRegex = /^[A-Za-z]+$/;
+            const alphaNumRegex = /^[A-Za-z0-9]+$/;
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const numericRegex = /^[0-9]+$/;
+            const validStatut = ['LEAD', 'CLIENT', 'PROSPECT'];
+            this.errors = [];
+
+            if (!this.contact.nom)
+                this.errors.push('The nom field is required.');
+            else if (!alphaRegex.test(this.contact.nom))
+                this.errors.push('The nom field must contain only alphabetic characters.');
+
+            if (!this.contact.prenom)
+                this.errors.push('The prenom field is required.');
+            else if (!alphaRegex.test(this.contact.prenom))
+                this.errors.push('The prenom field must contain only alphabetic characters.');
+
+            if (!this.contact.e_mail)
+                this.errors.push('The e_mail field is required.');
+            else if (!emailRegex.test(this.contact.e_mail))
+                this.errors.push('The e_mail field must be a valid email address.');
+
+            if (!this.contact.organisation.nom)
+                this.errors.push('The organisation nom field is required.');
+            else if (!alphaNumRegex.test(this.contact.organisation.nom))
+                this.errors.push('The organisation nom field must contain only alphabetic and numeric characters.');
+
+            if (this.contact.organisation.code_postal && !numericRegex.test(this.contact.organisation.code_postal))
+                this.errors.push('The organisation.code_postal field must contain only numeric characters.');
+
+            if (this.contact.organisation.ville && !alphaRegex.test(this.contact.organisation.ville))
+                this.errors.push('The organisation.ville field must contain only alphabetic characters.');
+
+            if (this.contact.organisation.statut && !validStatut.includes(this.contact.organisation.statut))
+                this.errors.push(`The organisation.statut field must be one of the following values: ${validStatut.join(', ')}.`);
         }
     }
 }
