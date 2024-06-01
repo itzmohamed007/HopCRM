77e72b3486a668b91ed4dcd8d558e28013c7a36e
mohamed bourra <itzmohamed007@gmail.com>
2024-06-01 22:30:13 +0100

    completed duplicate alert modal

--- frontend/src/components/DuplicateAlertModal.vue
+++ frontend/src/components/DuplicateAlertModal.vue
@@ -3,7 +3,9 @@
         <div class="relative top-4 left-32 w-3/5 bg-white rounded-md">
             <div class="p-5 text-start">
                 <div class="flex items-center justify-start gap-5">
-                    <span class="p-2 rounded-full bg-red-100 text-red-500"><Icon icon="octicon:alert-16" /></span>
+                    <span class="p-2 rounded-full bg-red-100 text-red-500">
+                        <Icon icon="octicon:alert-16" />
+                    </span>
                     <p class="text-xl font-semibold">Doublon</p>
                 </div>
                 <div class="py-10 px-14 text-sm font-semibold text-gray-500">
@@ -12,8 +14,10 @@
                 </div>
             </div>
             <div class="p-5 bg-gray-100 flex items-center justify-end gap-2 text-sm rounded-b-md">
-                <button class="px-5 py-2 rounded-md text-gray-900 bg-gray-100 border border-gray-300">Annuler</button>
-                <button class="px-5 py-2 rounded-md text-white bg-red-500 border border-gray-300">Confirmer</button>
+                <button @click="$emit('closeDuplicateAlertModal')"
+                    class="px-5 py-2 rounded-md text-gray-900 bg-gray-100 border border-gray-300">Annuler</button>
+                <button @click="$emit('validateDuplicateAlertModal')"
+                    class="px-5 py-2 rounded-md text-white bg-red-500 border border-gray-300">Confirmer</button>
             </div>
         </div>
     </div>
