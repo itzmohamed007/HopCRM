354eded0265ec3f0d21f2e8bb8c3f8e063afa443
mohamed bourra <itzmohamed007@gmail.com>
2024-06-03 14:45:38 +0100

    refactored endpoints file

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,12 +2,8 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\OrganisationController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::get('v1/contacts', [ContactController::class, 'index']);
 Route::get('v1/contacts/search/{target}', [ContactController::class, 'search']);
@@ -19,7 +15,8 @@ Route::delete('v1/contacts', [ContactController::class, 'destroy']);
 Route::post('v1/contacts/duplicate', [ContactController::class, 'isDuplicate']);
 Route::post('v1/organisations/duplicate', [OrganisationController::class, 'isDuplicate']);
 
-Route::fallback(function(){
+Route::fallback(function () {
     return response()->json([
-        'message' => 'Route Not Found'], 404);
+        'message' => 'Route Not Found'
+    ], 404);
 });
