7530588a5b9f635367a606543dd3405a88758660
mohamed bourra <itzmohamed007@gmail.com>
2024-06-03 14:44:30 +0100

    added CodeBlocks

--- backend/app/Repositories/Specifications/IContactRepository.php
+++ backend/app/Repositories/Specifications/IContactRepository.php
@@ -4,11 +4,38 @@ namespace App\Repositories\Specifications;
 
 interface IContactRepository
 {
+    /**
+     * Retrieve all contacts.
+     */
     public function index();
+
+    /**
+     * Retrieve a contact by its ID.
+     */
     public function getById($id);
+
+    /**
+     * Search for target by name, surname and organisation name
+     */
     public function search($target);
+
+    /**
+     * Check if a contact with the given name and surname is a duplicate.
+     */
     public function isDuplicate($nom, $prenom);
+
+    /**
+     * Store a new contact including associated organisation.
+     */
     public function store(array $data);
-    public function update(array $data, $id);
+
+    /**
+     * Update an existing contact including associated organisation.
+     */
+    public function update(array $data);
+
+    /**
+     * Delete a contact by its ID.
+     */
     public function delete($id);
 }
--- backend/app/Repositories/Specifications/IOrganisationRepository.php
+++ backend/app/Repositories/Specifications/IOrganisationRepository.php
@@ -4,7 +4,16 @@ namespace App\Repositories\Specifications;
 
 interface IOrganisationRepository
 {
+    /**
+     * Store a new store organisation.
+     */
     public function store(array $data);
+    /**
+     * Check if a organisation with the given name is a duplicate.
+     */
     public function isDuplicate($nom);
+    /**
+     * Update an existing organisation.
+     */
     public function update(array $data);
 }
