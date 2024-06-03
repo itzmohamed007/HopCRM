e33e2a8ad382e20cbbbb57bbee13d707671fdb1c
mohamed bourra <itzmohamed007@gmail.com>
2024-06-03 14:38:50 +0100

    renamed organisation table to single format

--- backend/database/migrations/2024_05_27_221258_create_organisation_table.php
+++ backend/database/migrations/2024_05_27_221258_create_organisation_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('organisations', function (Blueprint $table) {
+        Schema::create('organisation', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
             $table->softDeletes();
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('organisations');
+        Schema::dropIfExists('organisation');
     }
 };
